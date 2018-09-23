b3e2be2f37b16be397edab6abff7417b5b1a7621
simon1988 <simon.niu1988@gmail.com>
2018-09-23 11:16:55 +0800

    idb result process

--- idb_result_process.py
+++ idb_result_process.py
@@ -11,6 +11,13 @@ def strip_quote(str):
         return str[1:-1]
 
 
+def to_camel_case(snake_str):
+    components = snake_str.split('_')
+    # We capitalize the first letter of each component except the first one
+    # with the 'title' method and join them together.
+    return components[0] + ''.join(x.title() for x in components[1:])
+
+
 def idb(is_strip_quote):
     files = glob.glob("/Users/simon/Downloads/执行结果-*.txt")
     files.sort(key=os.path.getmtime, reverse=True)
@@ -43,6 +50,29 @@ def output_reg_doc(file):
         print(res)
 
 
-idb(True)
+def gen_sql_mapping(file):
+    with open('/Users/simon/Downloads/' + file, 'r') as f:
+        res = ''
+        for line in f:
+            line = line.strip()
+            res = res + "<result column=\"{0}\" 			property=\"{1}\" 			jdbcType=\"VARCHAR\" />\n".format(
+                line, to_camel_case(line))
+        print(res)
+
+
+def get_machine_list(file):
+    with open('/Users/simon/tmp/' + file, 'r') as f:
+        res = ''
+        for line in f:
+            line = line.strip()
+            end = line.find("(内网)");
+            if end >= 0:
+                res = res+line[:end]+"\n"
+        print(res)
+
+
+# idb(True)
 # idb(False)
 # output_reg_doc('执行结果-11.txt')
+# gen_sql_mapping('1.txt')
+get_machine_list('machine.txt')
